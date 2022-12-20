95a3e35fda62405597cadfa13b5626169e63d6f4
Tyson Tang <90588335+tysonking33@users.noreply.github.com>
2022-12-20 17:33:40 +1030

    WIP - beginning thread initalisation

--- using mutex/sources/main.c
+++ using mutex/sources/main.c
@@ -6,16 +6,6 @@ int	main(int argc, char **argv)
     gen = (t_rules *)malloc(sizeof(t_rules));
     if (!gen)
         return 0;
-    
-
-    
-    
-    
-
-    for (i = 0; i<5; i++)
-    {
-        pthread_join(T[i], NULL); //making the parent program wait for each thread to finish
-    }
 
 
     //initalise rules
@@ -26,32 +16,9 @@ int	main(int argc, char **argv)
 
 
     //initalise threads
-    init_threads(gen);
+    start_threads(gen);
 
-    //jointhreads
-    do_your_thing_21();
-}
+    //pthread lock
 
-void * philos(void * n)
-{
-    int ph=*(int *)n;
-    printf("Philosopher %d wants to eat\n",ph);
-    printf("Philosopher %d tries to pick left chopstick\n",ph);
-    sem_wait(&chopstick[ph]); //sem_wait() = picking up the chopstick
-    printf("Philosopher %d picks the left chopstick\n",ph);
-    printf("Philosopher %d tries to pick the right chopstick\n",ph);
-    sem_wait(&chopstick[(ph+1)%5]); // have to go around to the other side of the philopsher (5 is number of philosphers)
-    printf("Philosopher %d picks the right chopstick\n",ph);
-    eat(ph); //only occurs if both chopsticks are picked up
-    sleep(2); //pauses current thread for 2 seconds, allowng for other philosophers to eat
-    printf("Philosopher %d has finished eating\n",ph);
-    sem_post(&chopstick[(ph+1)%5]); // put down right chopstick
-    printf("Philosopher %d leaves the right chopstick\n",ph);
-    sem_post(&chopstick[ph]); // put down left chopstick
-    printf("Philosopher %d leaves the left chopstick\n",ph);
-}
-
-void eat(int ph)
-{
-    printf("Philosopher %d begins to eat\n",ph);
+    //pthread unlock
 }
---
+++ using mutex/sources/start_thread.c
@@ -0,0 +1,5 @@
+#include "../includes/philosophers.h"
+
+void    start_threads(t_rules *gen){
+    
+}
